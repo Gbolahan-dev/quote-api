b102fdf77058322d74abc9e380ae63f6df1cd78b
Gbolahan.dev <gbolahan.adekanbi.dev@gmail.com>
2025-04-24 05:47:25 -0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const quotes = require('./quotes.json');
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send("Welcome to the Quote API 😎");
+});
+
+app.get('/', (req, res) => {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  res.json({ quote: randomQuote });
+});
+
+app.get('/quotes', (req, res) =>{
+   res.json(quotes)
+});
+
+app.listen(PORT, () => {
+  console.log('Server running at http://localhost:${PORT}');
+});
