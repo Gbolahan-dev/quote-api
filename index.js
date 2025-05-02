d0bc10e8e891195112c1c6a4fd9386a8030fc4d6
Gbolahan.dev <gbolahan.adekanbi.dev@gmail.com>
2025-05-02 10:36:45 -0900

    Simulate crash on /quote

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-const express = require('express');
-const quotes = require('./quotes.json');
-
-const app = express();
-const PORT = process.env.PORT || 8000;
-
-app.get('/', (req, res) => {
-  res.send("Welcome to the Quote API v2 baby 😎");
-});
-
-
-app.get('/quote', (req, res) => {
-  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-  res.json({ quote: randomQuote });
-});
-
-app.get('/quotes', (req, res) =>{
-   res.json(quotes)
-});
-
-app.listen(PORT, () => {
- console.log(JSON.stringify({
-  severity: "INFO",
-  message: `Server running at http://localhost:${PORT}`,
-  serviceContext: {
-    service: "quote-api",
-  },
-}));
-
+app.get('/quote', (req, res) = {
+   throw new Error("Simulated Crash");
 });
