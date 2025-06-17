d62a416b1965a9b8f1eca3a69681dc2a4cbab4ac
Gbolahan.dev <gbolahan.adekanbi.dev@gmail.com>
2025-06-17 14:17:07 +0100

    updated

--- infra/helm.tf
+++ infra/helm.tf
@@ -8,12 +8,14 @@ resource "helm_release" "quote_api_staging_2" {
     name  = "image.repository"
     value = "${var.region}-docker.pkg.dev/${var.project_id}//${google_artifact_registry_repository.main_quote_api_repo.repository_id}/quote-api"
   }
+
   set {
     name  = "image.tag"
     value = "latest" # hard-coded test tag
   }
 
+ 
+
   depends_on = [google_container_node_pool.secure_pool_2]
 }
-
 
